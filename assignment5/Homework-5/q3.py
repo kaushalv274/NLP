294958138f68f56226278eea11211be665e386bd
Kaushal Shah <kaushalk@usc.edu>
2017-10-21 00:20:14 -0700

    assignment 5

---
+++ assignment5/Homework-5/q3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import distsim
+
+# you may have to replace this line if it is too slow 
+word_to_ccdict = distsim.load_contexts("nytcounts.4k")
+
+
+### provide your answer below
+ans = list()
+###Answer examples
+ans.append(distsim.show_nearest(word_to_ccdict, word_to_ccdict['miami'],set(['miami']),distsim.cossim_sparse))
+ans.append(distsim.show_nearest(word_to_ccdict, word_to_ccdict['doctor'],set(['doctor']),distsim.cossim_sparse))
+ans.append(distsim.show_nearest(word_to_ccdict, word_to_ccdict['giant'],set(['giant']),distsim.cossim_sparse))
+ans.append(distsim.show_nearest(word_to_ccdict, word_to_ccdict['agree'],set(['agree']),distsim.cossim_sparse))
+ans.append(distsim.show_nearest(word_to_ccdict, word_to_ccdict['terrorist'],set(['terrorist']),distsim.cossim_sparse))
+ans.append(distsim.show_nearest(word_to_ccdict, word_to_ccdict['hotel'],set(['hotel']),distsim.cossim_sparse))
+
+for item in ans:
+    print item
